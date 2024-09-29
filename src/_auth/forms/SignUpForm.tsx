2c27bc5fe509a1defa0f4149be36985e3615a9b8
Kaloyan <kthristow@gmail.com>
2024-09-29 17:16:32 +0300

    Create the liked post page.

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -22,10 +22,10 @@ import { SignupValidation } from '@/lib/validation'
 const SignUpForm = () => {
   const { toast } = useToast();
   const navigate = useNavigate();
-  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const { checkAuthUser } = useUserContext();
 
   const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount()
-  const { mutateAsync: signInAccount, isPending: isSigningIn } = useSignInAccount()
+  const { mutateAsync: signInAccount} = useSignInAccount()
 
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -8,7 +8,6 @@ const Home = () => {
   const {
     data: posts,
     isPending: isPostLoading,
-    isError: isErrorPosts,
   } = useGetRecentPosts();
 
   return (
