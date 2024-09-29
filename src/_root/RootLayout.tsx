226f9445f89457d7e957d9002ae9eaa181e4be91
Kaloyan <kthristow@gmail.com>
2024-09-29 17:35:04 +0300

    Update the imports

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -4,8 +4,6 @@ import Topbar from "@/components/shared/Topbar";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 
-
-
 const RootLayout = () => {
   return (
     <div className="w-full md:flex">
