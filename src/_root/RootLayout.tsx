8aea2ac781f8cd10933ade81ec5094783bba0c4f
Kaloyan <kthristow@gmail.com>
2024-09-29 17:24:00 +0300

    Update RootLayout.tsx

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Topbar from "@/components/shared/Topbar";
 import Bottombar from "@/components/shared/Bottombar";
-import LeftSidebar from "@/components/shared/LeftSidebar";
+import LeftSidebar from '../../src/components/shared/LeftSidebar'
 
 
 const RootLayout = () => {
